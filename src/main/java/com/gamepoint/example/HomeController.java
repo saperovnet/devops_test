ec78a0561e97815b6581791039a42116743338bf
saperovnet <saperovnet@users.noreply.github.com>
2019-06-05 10:17:52 +0300

    Test devel branch

--- src/main/java/com/gamepoint/example/HomeController.java
+++ src/main/java/com/gamepoint/example/HomeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("")
 public class HomeController {
-
     @RequestMapping("/up")
     public String up() {
         throw new RuntimeException("Error");
